5f47ee0a693e8e54d46d771d838b2ffdfbaaa526
aminu.kabunu <aminu.kabunu@xpresspayments.com>
2019-02-13 16:27:10 +0100

    Homework Assignment #1: Data Types

---
+++ song.js
@@ -0,0 +1,56 @@
+/*
+Homework Assignment #1: Data Types
+BY
+Kabunu Aminu Cincin
+*/
+var artist = "2face";
+var label = "2face";
+var albumName = "Grass to Grace";
+var title = "African Queen"
+var genre = "Pop"
+var duration_ms = 255349;
+var key = 5;
+var mode = 0;
+var time_signature = 4;
+var acousticness = 0.514;
+var danceability = 0.735;
+var energy = 0.578;
+var instrumentalness = 0.0902;
+var liveness = 0.159;
+var loudness = -11.840;
+var speechiness = 0.0461;
+var valence = 0.624;
+var uri = "https://trackurl.com/?grass-to-grace";
+var track_href = "https://trackurl.com/?grass-to-grac";
+var type = "audio_features";
+var releaseYear = "2010";
+var composerName = ["2face", "2baba"];
+var previews = true;
+var playParameters = [1, "playback"]
+var artwork = {name: "2face", year: 2010}
+
+console.log(artist);
+console.log(label);
+console.log(albumName);
+console.log(title);
+console.log(genre);
+console.log(duration_ms);
+console.log(key);
+console.log(mode);
+console.log(time_signature);
+console.log(acousticness);
+console.log(danceability);
+console.log(energy);
+console.log(instrumentalness);
+console.log(liveness);
+console.log(loudness);
+console.log(speechiness);
+console.log(valence);
+console.log(uri);
+console.log(track_href);
+console.log(type);
+console.log(releaseYear);
+console.log(composerName);
+console.log(previews);
+console.log(playParameters);
+console.log(artwork);
